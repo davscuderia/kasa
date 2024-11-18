c382ab61c7c8c177afcbe2a50aaa76aa8eeb618c
Dav Prat <scuderia1980@gmail.com>
2024-11-18 22:31:50 +0100

    banner ok, début des cards

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,13 @@
+import React from "react"
 import Header from "../Header"
 import Footer from "../Footer"
-
+import './style.scss'
 
 const Layout = ({ children}) => (
     <>
-    <Header />
-    <main className="main-container">{children}</main>
-    <Footer />
+        <Header />
+        <main className="main-container">{children}</main>
+        <Footer />
     </>
 );
 
